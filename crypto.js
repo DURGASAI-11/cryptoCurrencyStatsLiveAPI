f08a51ce136a87eb53e0f71b7413e193e8418951
durgasai gogi <durgasaigogi11@gmail.com>
2023-08-18 13:28:25 +0530

    added new endpoints

--- crypto.js
+++ crypto.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const cryptoLinks = [
+const cryptoLinks1 = [
   'bitcoin',
   'ethereum',
   'cardano',
@@ -27,6 +27,9 @@ const cryptoLinks = [
   'avalanche',
   'unus-sed-leo',
   'chainlink',
+]
+
+const cryptoLinks2 = [
   'stellar',
   'binance-usd',
   'monero',
@@ -47,6 +50,8 @@ const cryptoLinks = [
   'optimism-ethereum',
   'the-graph',
   'xdc-network',
+]
+const cryptoLinks3 = [
   'algorand',
   'axie-infinity',
   'multiversx-egld',
@@ -67,6 +72,8 @@ const cryptoLinks = [
   'neo',
   'rocket-pool',
   'kava',
+]
+const cryptoLinks4 = [
   'kucoin-token',
   'flow',
   'paxos-standard',
@@ -87,6 +94,9 @@ const cryptoLinks = [
   'huobi-token',
   'terra-luna',
   'gatetoken',
+]
+
+const cryptoLinks5 = [
   'sui',
   'trust-wallet-token',
   'gemini-dollar',
@@ -114,10 +124,10 @@ app.get('/', (req, res) => {
   res.json('Welcome to the cryptocurrency API')
 })
 
-app.get('/cryptocurrencies', async (req, res) => {
+app.get('/cryptocurrenciesTop20', async (req, res) => {
   try {
     const articles = await Promise.all(
-      cryptoLinks.map(async (cryptoLink) => {
+      cryptoLinks1.map(async (cryptoLink) => {
         try {
           const response = await axios.get(
             `https://coinmarketcap.com/currencies/${cryptoLink}/`,
@@ -166,7 +176,239 @@ app.get('/cryptocurrencies', async (req, res) => {
         }
       }),
     )
+    res.json(articles.filter((article) => article !== null))
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
 
+app.get('/cryptocurrencies20to40', async (req, res) => {
+  try {
+    const articles = await Promise.all(
+      cryptoLinks2.map(async (cryptoLink) => {
+        try {
+          const response = await axios.get(
+            `https://coinmarketcap.com/currencies/${cryptoLink}/`,
+          )
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          const symbolElement = $('span.cjeUNx')
+          const priceElement = $('span.dxubiK')
+          const elementName = $('span.iSrqCq')
+
+          const symbol = symbolElement.text().trim()
+          const price = priceElement.text().trim()
+          const name = elementName.text().trim()
+
+          const priceElements = $('dd.fRWxhs', html)
+          const prices = priceElements
+            .toArray()
+            .map((element) => $(element).text().trim())
+
+          const marketCapital = prices[0].split('$')[1]
+          const volume24hrs = prices[1].split('$')[1]
+          const volumeOrMarketCap24hrs = prices[2]
+          const circulatingSupply = prices[3]
+          const totalSupply = prices[4]
+          const maxSupply = prices[5]
+          const fullyDilutedMarketCap = prices[6]
+          const source = 'coinmarketcap'
+
+          return {
+            symbol,
+            price,
+            name,
+            marketCapital,
+            volume24hrs,
+            volumeOrMarketCap24hrs,
+            circulatingSupply,
+            totalSupply,
+            maxSupply,
+            fullyDilutedMarketCap,
+            source,
+          }
+        } catch (error) {
+          console.error('Error:', error)
+          return null
+        }
+      }),
+    )
+    res.json(articles.filter((article) => article !== null))
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+app.get('/cryptocurrencies40to60', async (req, res) => {
+  try {
+    const articles = await Promise.all(
+      cryptoLinks3.map(async (cryptoLink) => {
+        try {
+          const response = await axios.get(
+            `https://coinmarketcap.com/currencies/${cryptoLink}/`,
+          )
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          const symbolElement = $('span.cjeUNx')
+          const priceElement = $('span.dxubiK')
+          const elementName = $('span.iSrqCq')
+
+          const symbol = symbolElement.text().trim()
+          const price = priceElement.text().trim()
+          const name = elementName.text().trim()
+
+          const priceElements = $('dd.fRWxhs', html)
+          const prices = priceElements
+            .toArray()
+            .map((element) => $(element).text().trim())
+
+          const marketCapital = prices[0].split('$')[1]
+          const volume24hrs = prices[1].split('$')[1]
+          const volumeOrMarketCap24hrs = prices[2]
+          const circulatingSupply = prices[3]
+          const totalSupply = prices[4]
+          const maxSupply = prices[5]
+          const fullyDilutedMarketCap = prices[6]
+          const source = 'coinmarketcap'
+
+          return {
+            symbol,
+            price,
+            name,
+            marketCapital,
+            volume24hrs,
+            volumeOrMarketCap24hrs,
+            circulatingSupply,
+            totalSupply,
+            maxSupply,
+            fullyDilutedMarketCap,
+            source,
+          }
+        } catch (error) {
+          console.error('Error:', error)
+          return null
+        }
+      }),
+    )
+    res.json(articles.filter((article) => article !== null))
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+app.get('/cryptocurrencies60to80', async (req, res) => {
+  try {
+    const articles = await Promise.all(
+      cryptoLinks4.map(async (cryptoLink) => {
+        try {
+          const response = await axios.get(
+            `https://coinmarketcap.com/currencies/${cryptoLink}/`,
+          )
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          const symbolElement = $('span.cjeUNx')
+          const priceElement = $('span.dxubiK')
+          const elementName = $('span.iSrqCq')
+
+          const symbol = symbolElement.text().trim()
+          const price = priceElement.text().trim()
+          const name = elementName.text().trim()
+
+          const priceElements = $('dd.fRWxhs', html)
+          const prices = priceElements
+            .toArray()
+            .map((element) => $(element).text().trim())
+
+          const marketCapital = prices[0].split('$')[1]
+          const volume24hrs = prices[1].split('$')[1]
+          const volumeOrMarketCap24hrs = prices[2]
+          const circulatingSupply = prices[3]
+          const totalSupply = prices[4]
+          const maxSupply = prices[5]
+          const fullyDilutedMarketCap = prices[6]
+          const source = 'coinmarketcap'
+
+          return {
+            symbol,
+            price,
+            name,
+            marketCapital,
+            volume24hrs,
+            volumeOrMarketCap24hrs,
+            circulatingSupply,
+            totalSupply,
+            maxSupply,
+            fullyDilutedMarketCap,
+            source,
+          }
+        } catch (error) {
+          console.error('Error:', error)
+          return null
+        }
+      }),
+    )
+    res.json(articles.filter((article) => article !== null))
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+app.get('/cryptocurrencies80to100', async (req, res) => {
+  try {
+    const articles = await Promise.all(
+      cryptoLinks5.map(async (cryptoLink) => {
+        try {
+          const response = await axios.get(
+            `https://coinmarketcap.com/currencies/${cryptoLink}/`,
+          )
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          const symbolElement = $('span.cjeUNx')
+          const priceElement = $('span.dxubiK')
+          const elementName = $('span.iSrqCq')
+
+          const symbol = symbolElement.text().trim()
+          const price = priceElement.text().trim()
+          const name = elementName.text().trim()
+
+          const priceElements = $('dd.fRWxhs', html)
+          const prices = priceElements
+            .toArray()
+            .map((element) => $(element).text().trim())
+
+          const marketCapital = prices[0].split('$')[1]
+          const volume24hrs = prices[1].split('$')[1]
+          const volumeOrMarketCap24hrs = prices[2]
+          const circulatingSupply = prices[3]
+          const totalSupply = prices[4]
+          const maxSupply = prices[5]
+          const fullyDilutedMarketCap = prices[6]
+          const source = 'coinmarketcap'
+
+          return {
+            symbol,
+            price,
+            name,
+            marketCapital,
+            volume24hrs,
+            volumeOrMarketCap24hrs,
+            circulatingSupply,
+            totalSupply,
+            maxSupply,
+            fullyDilutedMarketCap,
+            source,
+          }
+        } catch (error) {
+          console.error('Error:', error)
+          return null
+        }
+      }),
+    )
     res.json(articles.filter((article) => article !== null))
   } catch (error) {
     console.error('Error:', error)
@@ -226,6 +468,7 @@ app.get('/cryptocurrencies/:cryptoName', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' })
   }
 })
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
